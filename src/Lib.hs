65e4a89d95c01831300a928a406d0df4b592d344
Astronomax <fgfgfb93@gmail.com>
2022-03-13 21:21:52 +0300

    Implement solver interface

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,14 @@
 module Lib (someFunc) where
 import GenerateTask (generateTask)
+import PrintSolution (printSolution)
+import System.Environment (getArgs)
 import Picosat
 
 someFunc :: IO ()
 someFunc = do
-    res <- solve $ generateTask 8
+    args <- getArgs
+    let n = (read $ head args) :: Int
+    res <- solve $ generateTask n
     case res of
-        (Solution s) -> putStrLn $ show $ filter (>0) s
+        (Solution s) -> putStrLn $ printSolution n (filter (>0) s)
         _ -> putStrLn "The problem has no solutions"
---
+++ src/PrintSolution.hs
@@ -0,0 +1,6 @@
+module PrintSolution (printSolution) where
+
+printSolution :: Int -> [Int] -> String
+printSolution n s = foldl1 (\x y -> x ++ "\n" ++ y) $ 
+    (\x -> "|" ++ (replicate x '.') ++ "Q" ++ (replicate (n - x - 1) '.') ++ "|") 
+    <$> (flip mod n . pred) <$> s 
